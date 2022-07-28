7767c990a9f456f1448c88babfff238174f20dc5
Kene-M <109693458+Kene-M@users.noreply.github.com>
2022-07-28 17:15:38 -0400

    Add files via upload

---
+++ webScraper/webScraper.py
@@ -0,0 +1,17 @@
+import requests 
+from bs4 import BeautifulSoup
+
+url = "https://www.geeksforgeeks.org/"
+#url = "https://www.facebook.com/"
+
+html = requests.get(url)
+doc = BeautifulSoup(html.text, "html.parser")
+
+titleHeader = doc.find("title")
+#titleHeader = doc.title
+
+title = titleHeader.text
+#title = titleHeader.string
+
+print(doc.prettify())
+#print(title)
